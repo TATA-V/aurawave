0a2ec431bb28065a212c3e9da6a2fb4cf2c9959a
Choi-hyeona <xhrl523@naver.com>
2024-02-07 23:36:28 +0900

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     styledComponents: true,
   },
   images: {
-    loader: 'imgix',
     domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
   },
   webpack(config) {
